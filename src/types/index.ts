d51771f6d235dae7355339007f45b9c4f5715527
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-04-15 11:48:12 -0600

    Add initial WebSocket server implementation with environment configuration, message handling, and client interaction

---
+++ src/types/index.ts
@@ -0,0 +1,69 @@
+//! Este es el objeto que se almacena por cada cliente
+export interface WebSocketData {
+  clientId: string;
+  name: string;
+  color: string;
+  coords: LatLng;
+}
+
+export interface LatLng {
+  lat: number;
+  lng: number;
+}
+
+export interface ClientMarker {
+  clientId: string;
+  name: string;
+  color: string;
+  coords: LatLng;
+  updatedAt: number;
+}
+
+export type IncomingWsMessage =
+  |
+  {
+    type: 'CLIENT_REGISTER',
+    payload: { name: string; color: string; coords: LatLng; }
+  }
+  |
+  {
+    type: 'CLIENT_MOVE',
+    payload: { coords: LatLng; }
+  }
+  |
+  {
+    type: 'GET_CLIENTS',
+    payload?: any
+  }
+
+export type OutgoingWsMessage =
+  |
+  {
+    type: 'ERROR',
+    payload: { error: string; }
+  }
+  |
+  {
+    type: 'WELCOME',
+    payload: { clientId: string; }
+  }
+  |
+  {
+    type: 'CLIENT_STATE',
+    payload: ClientMarker[];
+  }
+  |
+  {
+    type: 'CLIENT_JOIN',
+    payload: ClientMarker;
+  }
+  |
+  {
+    type: 'CLIENT_MOVED',
+    payload: { clientId: string; coords: LatLng; updatedAt: number }
+  }
+  |
+  {
+    type: 'CLIENT_LEFT',
+    payload: { clientId: string; }
+  }
